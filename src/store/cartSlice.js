5cd5ed6b6d94b917d433bdde8bd6e627b646a083
JerryU <jerry.uusitalo@gmail.com>
2024-01-31 21:43:47 +0200

    Add comments

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,19 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+    // Creating a Redux slice named "cart"
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
         cart: []
     },
+    // Reducers to handle actions and update the state
     reducers: {
         addToCart: (state, action) => {
+            // Update the 'cart' array by adding the payload to it
             state.cart = [...state.cart, action.payload]
         },
-        // Imagine the above as a function
-        // Imagine it looks like:
-        // const addToCart = (state, action) => {
-        // setCart([..cart, action.payload])
-        //}
+    // Additional reducers can be added here for other actions
     },
     extraReducers: (builder) => { },
 })
